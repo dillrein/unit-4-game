4432643b1ededb6e90ea67abcdf1e7d3239666c2
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-08-20 21:16:19 -0700

    jquery and ifs

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,22 +1,48 @@
-//VARIABLES
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+$(document).ready(function(){
+    var wins = 0;
+    var loses = 0;
+    var scoreNum = Math.floor((Math.random() * 120) +19); //comp random number
+    var beerNum = Math.floor((Math.random() * 12) +1); //button number generator
+   
+    function start(){
+            //show random number for game and assign vaule
+        $("#ranNum").append(scoreNum).val(scoreNum)
+        console.log(ranNum.value)
 
-var scoreNum = Math.floor((Math.random() * 120) +19); //comp random number
-var crystalNum = Math.floor((Math.random() * 12) +1); //button number generator
+        $("#button1").val(beerNum)
+        console.log(button1.value + " b1")
 
-
-
-//FUNCTIONS
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-$
-
-
-
-//METHOD
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
+        
+        $(".button").on("click", function(){
+            
 
 
-s
+        })
+    }
+
+    if(ranNum.value === userNum.value){
+        wins++;
+        $("winsDisplay").html() = wins;
+
+        console.log(winsDisplay)
+
+    }else if(userNum.value > ranNum.value){
+        losses--;
+        $("losesDisplay").html() = loses;
+
+        console.log(losesDisplay)
+    }
+
+
+
+    
+    
+    
+    
+    
+    
+    
+
+
+
+})
